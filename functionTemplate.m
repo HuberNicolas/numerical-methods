2b2f152537962a3c72ae8c27749ed4c890abf01a
Nicolas Huber <56343633+HuberNicolas@users.noreply.github.com>
2020-12-15 09:06:32 +0100

    Added function Template

---
+++ functionTemplate.m
@@ -0,0 +1,16 @@
+function [x,y,z] = nameOftheFunction(a,b,c)
+%   Description of the function
+%   
+%   Input:
+%           a        : a Parameter
+%           b        : a Parameter
+%           c        : a Parameter
+%
+%   Output:
+%           x        : a return Value
+%           y        : a return Value
+%           z        : a return Value
+
+
+% Add code here of the function
+  
