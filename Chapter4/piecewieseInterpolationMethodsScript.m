a762607a1bb0d029ab3050e30afc92d859f091ff
Nicolas Huber <56343633+HuberNicolas@users.noreply.github.com>
2021-01-05 13:28:38 +0100

    Added quadratic Spline method

--- Chapter4/piecewieseInterpolationMethodsScript.m
+++ Chapter4/piecewieseInterpolationMethodsScript.m
@@ -17,10 +17,13 @@ clc; clear all; close all;
 x = [3;4.5;7;9];    % dataset
 y = [2.5;1;2.5;0.5];    % dataset
 % secure
-xeval = [6,9,5,7,3]; % where to evaluate
+xeval      = [6,9,5,7,3]; % where to evaluate
+xevalQuick = [3;5;6;7;9]; % where to evaualte for interp1q 
 
 
 %% Computation
-yeval1 = linearPiecewiseInterpolation(x,y,xeval);
-yeva2 = interp1(x,y,xeval); % integrated Matlab solution
+yevall = linearPiecewiseInterpolation(x,y,xeval);
+yeval2 = interp1(x,y,xeval); % integrated Matlab solution
+yeval3 = interp1q(x,y,xevalQuick); % integrated Matlab solution, faster
+S = quadraticSplineInterpolation(x,y);
 %% Plotting
