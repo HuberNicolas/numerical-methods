5f2cbb19fc58f330602fd3b18ac0f5e1bba58eef
Nicolas Huber <56343633+HuberNicolas@users.noreply.github.com>
2021-01-07 15:18:16 +0100

    Added Jacobi, GaussSeidel und GaussSeidWeighting iterative methods

--- Chapter7/scriptTemplate.m
+++ Chapter7/scriptTemplate.m
@@ -6,15 +6,20 @@
 %}
 %% About
 %{
-    
+    Skript for iterative methods
 %}
 
 %% Clearing Workspace
 clc; clear all; close all;
 
 %% Initialisation
-
+A = [3, -0.1, -0.2; 0.1, 7, -0.3; 0.3, -0.2, 10]
+b = [7.85; -19.3; 71.4]
+x0 = [0;0;0]
+itMax = 10000;
+eps = 10e-10;
+w = 1;
 %% Computation
-
+[x,it] = gaussSeidelMethodWeighting(A,b,x0,itMax,eps,w);
 %% Plotting
 
